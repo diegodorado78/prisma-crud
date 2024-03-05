56311b921bbb48efe91220f29fd05a784a8a087b
diegodorado78 <diegodorado78@gmail.com>
2024-03-05 16:39:16 -0500

    updated model

--- src/controllers/user/index.ts
+++ src/controllers/user/index.ts
@@ -9,29 +9,40 @@ const prisma = new PrismaClient()
 export const getUsers = async (req: Request, res: Response) => {
     try {
         const users = await prisma.user.findMany();
-        return res.send(users)
+        !users && res.status(400).send({ message: 'Users not found' });
+        return res.json(users)
     } catch (error) {
         console.error('Error getting users:', error);
     }
 };
 
 
-// Get  user id
+// Get  user by id
 export const getUserById = async (req: Request, res: Response) => {
-
     const { id } = req.params
     try {
-        const validation = IdParamSchema.parse(req.params)
-        console.log(validation);
+        const validation = IdParamSchema.safeParse(req.params)
+        console.log(validation.success);
         const user = await prisma.user.findUnique({ where: { id: parseInt(id) } });
-        if (user) res.send(user);
-        else return res.status(400).send({ message: 'User not found' });
+        !user && res.status(400).send({ message: 'User not found' });
+        return res.json(user);
     } catch (e) {
-        console.log(e);
+        return res.status(400).send((e as Error).message);
+    }
+};
 
-        const user = await prisma.user.findUnique({ where: { id: parseInt(id) } });
-        if (user) return res.send(user);
+// Delete  user by id
+export const deleteUserById = async (req: Request, res: Response) => {
+    const { id } = req.params
+    try {
+        const validation = IdParamSchema.safeParse(req.params)
+        console.log(validation.success);
+        const userDeleted = await prisma.user.delete({ where: { id: parseInt(id) } });
+        console.log(userDeleted);
 
-        //return res.status(400).send((e as Error).message);
+        !userDeleted && res.status(400).send({ message: 'User to delete not found' });
+        return res.json(userDeleted);
+    } catch (e) {
+        return res.status(400).send((e as Error).message);
     }
 };
--- src/routes/User.routes.ts
+++ src/routes/User.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express'
-import { getUserById, getUsers } from '../controllers/user'
-import { IdParamSchema } from '../controllers/validations';
+import { deleteUserById, getUserById, getUsers } from '../controllers/user'
 
 const router = Router()
 
 router.get('/users', getUsers)
 
 router.get('/users/id/:id', getUserById);
+
+router.delete('/users/id/:id', deleteUserById);
 
 
 export default router

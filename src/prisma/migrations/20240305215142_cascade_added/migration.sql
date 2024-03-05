c9d02826ae13ce29dd5f706f133d82b12264800c
diegodorado78 <diegodorado78@gmail.com>
2024-03-05 16:53:15 -0500

    cascade added

---
+++ src/prisma/migrations/20240305215142_cascade_added/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `authorId` on the `Post` table. All the data in the column will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Profile" DROP CONSTRAINT "Profile_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "Post" DROP COLUMN "authorId",
+ADD COLUMN     "UserId" INTEGER;
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_UserId_fkey" FOREIGN KEY ("UserId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
